2bacc2e037617e91149a53420fac5fa2b9c61327
Muntasir12 <mixono6437@v2ssr.com>
2023-09-01 11:17:07 +0600

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Title";
+
+const Banner = () => {
+  return (
+    <div>
+      <Title test="Title for Banner" />
+    </div>
+  );
+};
+
+export default Banner;
